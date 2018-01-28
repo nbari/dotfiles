1804599c5f717e75a8a9088cf520297a603f9bae
Nicolas Embriz <nbari@tequila.io>
2018-01-28 19:45:48 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,6 +21,36 @@ set -e
 #         the length of STRING is zero
 # ----------------------------------------------------------------------------
 
+brew update
+
+brew install ag && \
+brew install autossh && \
+brew install direnv && \
+brew install python && \
+brew install vim --with-override-system-vi && \
+brew install git --without-completions && \
+brew install npm && \
+brew install ripgrep && \
+brew install ssh-vault && \
+brew install tmux && \
+brew install tree && \
+brew install zsh
+
+if grep -Fxq "/usr/local/bin/zsh" /etc/shells
+then
+    echo "zsh found"
+else
+    echo "/usr/local/bin/zsh" | sudo tee -a /etc/shells
+fi
+
+# change default shell
+if finger $USER | grep -q "/usr/local/bin/zsh"
+then 
+    echo "using zsh"
+else 
+    chsh -s /usr/local/bin/zsh
+fi
+
 cd $home
 
 # now=$(date -u +%Y%m%dT%H%M%S)
@@ -103,9 +133,8 @@ hash npm >/dev/null 2>&1 && env npm install js-beautify js-yaml jsonlint less js
 }
 
 echo "Installing pip packages..."
-hash pip >/dev/null 2>&1 && env pip install --user --upgrade autopep8 pyflakes pylint cryptography || {
+hash pip >/dev/null 2>&1 && env pip install --user --upgrade ansible pipenv autopep8 pyflakes pylint cryptography || {
     echo "pip not installed (python -m ensurepip --upgrade)"
 }
 
-echo "To change your default shell to zsh:  chsh -s `which zsh`"
 echo "Fin"
