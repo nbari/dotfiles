7da99e3a29757115dcc65a1f36a64bb9384f7a85
nbari <nbari@tequila.io>
2018-01-28 20:31:58 +0100

    sync Sun Jan 28 20:31:58 CET 2018

--- install.sh
+++ install.sh
@@ -26,14 +26,15 @@ brew update
 brew install ag && \
 brew install autossh && \
 brew install direnv && \
-brew install python && \
-brew install vim --with-override-system-vi && \
 brew install git --without-completions && \
 brew install npm && \
+brew install python && \
+brew install reattach-to-user-namespace \
 brew install ripgrep && \
 brew install ssh-vault && \
 brew install tmux && \
 brew install tree && \
+brew install vim --with-override-system-vi && \
 brew install zsh
 
 if grep -Fxq "/usr/local/bin/zsh" /etc/shells
