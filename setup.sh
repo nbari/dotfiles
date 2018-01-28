03df7230c260cc26ea3ca44bbb493ff87d16362b
Nicolas Embriz <nbari@tequila.io>
2018-01-28 19:07:55 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,23 @@
 set -e
 
+brew install ag && \
+brew install autossh && \
+brew install direnv && \
 brew install python && \
 brew install vim --with-override-system-vi && \
 brew install git --without-completions && \
+brew install npm && \
+brew install ripgrep && \
+brew install ssh-vault && \
+brew install tmux && \
+brew install tree && \
 brew install zsh
+
+if grep -Fxq "/usr/local/bin/zsh" /etc/shells
+then
+else
+  echo "/usr/local/bin/zsh" >> /etc/shells
+fi
+
+# change default shell
+chsh -s /usr/local/bin/zsh
