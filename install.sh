d59ea9a46131182c71633b9c8c7ae0b3c7127e63
nbari <nbari@tequila.io>
2018-09-11 12:35:08 +0200

    update Tue Sep 11 12:35:08 CEST 2018

--- install.sh
+++ install.sh
@@ -54,6 +54,7 @@ brew install ssh-vault && \
 brew install tmux && \
 brew install tree && \
 brew install vim --with-override-system-vi && \
+brew install watch && \
 brew install zsh
 
 if grep -Fxq "/usr/local/bin/zsh" /etc/shells
