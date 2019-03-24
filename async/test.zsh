111818d822bdabb6dd50af6b94f25a2106580063
nbari <nbari@tequila.io>
2019-03-24 13:37:45 +0100

    sync Sun Mar 24 13:37:45 CET 2019

---
+++ async/test.zsh
@@ -0,0 +1,32 @@
+# ----------------------------------------------------------------------------
+# Put custom alias per host on .zshenv
+# ----------------------------------------------------------------------------
+
+my_status(){
+    if [ -n "$(git rev-parse --is-inside-work-tree 2>/dev/null)" ]; then
+        git rev-parse --abbrev-ref HEAD
+    fi
+}
+
+my_callback(){
+    OUT=$3
+    async_job my_worker my_status
+}
+
+autoload -Uz async && async
+async_init
+
+async_start_worker "my_worker" -n
+async_register_callback my_worker my_callback
+async_job "my_worker" my_status
+
+TMOUT=1
+TRAPALRM() { zle reset-prompt }
+
+date_string="%D{%Y-%m-%d %H:%M:%S}"
+username="%n"
+path_string="%3c"
+precmd() {
+    print -rP "${date_string} ${username} ${path_string} ${OUT}"
+}
+PROMPT='» '
