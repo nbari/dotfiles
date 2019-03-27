64af2d535b311fc89c6e8e186b38a3307a106037
nbari <nbari@tequila.io>
2019-03-27 16:11:39 +0100

    sync Wed Mar 27 16:11:39 CET 2019

--- async/trap.zsh
+++ async/trap.zsh
@@ -1,29 +1,23 @@
-#
-autoload -Uz async && async
-async_init
 typeset -Ag prompt_data
 
 function zle-line-init zle-keymap-select prompt_refresh {
     PROMPT="$prompt_data[out] >> "
-    zle reset-prompt
+    zle && zle reset-prompt
 }
 
 zle -N zle-line-init
 zle -N zle-keymap-select
+
 
 prompt_git(){
     sleep 3
     if [ -n "$(cd $1 && git rev-parse --is-inside-work-tree 2>/dev/null)" ]; then
       prompt_data[out]=$(git rev-parse --abbrev-ref HEAD)
     fi
+    prompt_refresh
 }
 
 prompt_precmd() {
-    callback() {
-        echo 11111
-        prompt_refresh
-    }
-    trap callback EXIT
     prompt_data[out]=""
     prompt_git $(pwd) &!
 }
