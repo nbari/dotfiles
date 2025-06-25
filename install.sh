d5952c293bafe7058843abb0972dca0547cfe60c
nbari <nbari@tequila.io>
2025-06-25 23:37:00 +0200

    sync Wed Jun 25 23:37:00 CEST 2025

--- install.sh
+++ install.sh
@@ -18,6 +18,12 @@ brew update
 
 defaults write com.apple.screencapture location ~/Screenshots
 
+# Create Screenshots directory if it doesn't exist
+if [ ! -d ~/Screenshots ]; then
+    echo "making Screenshots directory"
+    mkdir ~/Screenshots
+fi
+
 cd "$HOME"
 
 # create Bundle file
@@ -32,6 +38,8 @@ brew "ipcalc"
 brew "jq"
 brew "lz4"
 brew "neovim"
+brew "node"
+brew "pnpm"
 brew "ripgrep"
 brew "rsync"
 brew "sipcalc"
@@ -85,6 +93,22 @@ if [ ! -d ~/.config/alacritty ]; then
 
     # Download Alacritty config
     curl -o ~/.config/alacritty/alacritty.toml -fsSL https://raw.githubusercontent.com/nbari/dotfiles/refs/heads/master/alacritty/alacritty.toml
+fi
+
+# Create .tmux.conf if it doesn't exist
+curl -o ~/.tmux.conf -fsSL https://raw.githubusercontent.com/nbari/dotfiles/refs/heads/master/my-dotfiles/.tmux.conf || {
+    echo "Failed to download .tmux.conf"
+    exit 1
+}
+
+# Create .zshrc if it doesn't exist
+if [ ! -f ~/.zshrc ]; then
+    echo "making .zshrc file"
+    curl -o ~/.zshrc -fsSL https://raw.githubusercontent.com/nbari/dotfiles/refs/heads/master/my-dotfiles/.zshrc || {
+        echo "Failed to download .zshrc"
+        exit 1
+    }
+    zinit
 fi
 
 echo "Fin"
